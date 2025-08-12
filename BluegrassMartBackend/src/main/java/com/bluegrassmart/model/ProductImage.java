42b8b692307939f51210a115e7a9d2e9039d13d3
keerthanaces <keerthanacse4@gmail.com>
2025-08-12 07:55:16 -0400

    backend file commit

---
+++ BluegrassMartBackend/src/main/java/com/bluegrassmart/model/ProductImage.java
@@ -0,0 +1,49 @@
+package com.bluegrassmart.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class ProductImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String imageName;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getImageName() {
+		return imageName;
+	}
+
+	public void setImageName(String imageName) {
+		this.imageName = imageName;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+    // Getters and setters
+    
+}
+
---
+++ BluegrassMartBackend/src/main/java/com/bluegrassmart/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.bluegrassmart.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.bluegrassmart.model.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+	 Optional<Product> findByName(String name);
+}
